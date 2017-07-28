2aefc0bbc266d19073786533b2d149757ddd8087
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-28 16:04:24 -0500

    add FBCreds file and prepare firebase info to be linked

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
 let mushApp = angular.module("MushroomMania", ["ngRoute"])
-.constant("FirebaseUrl", "https://todo-app-19fec.firebaseio.com/");
+.constant("FirebaseUrl", ".../");
 
 
