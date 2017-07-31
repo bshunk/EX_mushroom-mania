07d69937dcecf6670dadf0fbc85539a7e8216e1e
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-31 13:09:10 -0500

    get page to display without erros and have radio buttons work yet not display anything

--- app/factories/mushroom-factory.js
+++ app/factories/mushroom-factory.js
@@ -1,10 +1,10 @@
 'use strict';
 
-mushApp.factory("MushroomFactory", function($q, $http, FirebaseUrl, FBCreds) {
+mushApp.factory("MushroomFactory", function($q, $http, FirebaseUrl) {
   
   let getMushrooms = () => {
     return $q( (resolve, reject) => {
-      $http.get(`${FirebaseUrl}mushrooms.js`)
+      $http.get(`${FirebaseUrl}mushrooms.json`)
       .then( (mushrooms) => {
         resolve(mushrooms);
       })
