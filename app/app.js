42c2c631c8b35beffe929a7d0b8d92423184a861
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-31 10:51:44 -0500

    continue to try and get json files to store in firebase, and get radio buttons to display on page

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let mushApp = angular.module("MushroomMania", ["ngRoute"])
-.constant("FirebaseUrl", ".../");
+let mushApp = angular.module("MushroomMania", [])
+.constant("FirebaseUrl", "https://mushroommania-33d32.firebaseio.com/");
 
 
